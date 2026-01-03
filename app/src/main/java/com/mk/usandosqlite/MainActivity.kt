17c671ab27e4b464f54bbafc30ec711d48822228
Marlon Kososki <marlon.kososki@gmail.com>
2026-01-03 20:59:49 -0300

    feat: Initial project setup with SQLite and View Binding
    - Sets up the initial Android Studio project configuration files.
    - Enables View Binding in the app's `build.gradle.kts`.
    - Implements View Binding in `MainActivity`.
    - Adds basic code to create or open an SQLite database named `dbfile.sqlite`.
    - Renames the `btAlterOnClick` method to `btAlterarOnClick` for consistency.

--- app/src/main/java/com/mk/usandosqlite/MainActivity.kt
+++ app/src/main/java/com/mk/usandosqlite/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.mk.usandosqlite
 
+import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.mk.usandosqlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private lateinit var banco: SQLiteDatabase
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        banco = SQLiteDatabase.openOrCreateDatabase(
+            "dbfile.sqlite",
+            null
+            )
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
@@ -20,10 +36,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun btIncluirOnClick(view: View) {}
-    fun btAlterOnClick(view: View) {}
     fun btAlterarOnClick(view: View) {}
     fun btExcluirOnClick(view: View) {}
     fun btPesquisarOnClick(view: View) {}
-    fun btOnClick(view: View) {}
     fun btListarOnClick(view: View) {}
 }
