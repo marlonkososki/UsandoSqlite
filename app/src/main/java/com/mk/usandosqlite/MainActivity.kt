74dab92218e130f2461128e44912541a1da6bb80
Marlon Kososki <marlon.kososki@gmail.com>
2026-01-11 21:27:38 -0300

    feat: Implement dialog-based search in `MainActivity`
    - Replaces the direct search logic in `btPesquisarOnClick` with an `AlertDialog` to prompt for a record ID.
    - Adds an `EditText` inside the dialog to capture the user input for the search.
    - Updates the UI fields (`etCod`, `etNome`, and `etTelefone`) with the retrieved record data upon a successful search.
    - Clears all input fields and displays a `Toast` message if the record is not found.
    - Sets the dialog as non-cancelable and includes a "Fechar" button to dismiss it.

--- app/src/main/java/com/mk/usandosqlite/MainActivity.kt
+++ app/src/main/java/com/mk/usandosqlite/MainActivity.kt
@@ -5,8 +5,10 @@ import android.content.Intent
 import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -99,18 +101,37 @@ class MainActivity : AppCompatActivity() {
     }
     fun btPesquisarOnClick(view: View) {
 
-        val registro = banco.pesquisar(binding.etCod.text.toString().toInt())
 
-        if (registro != null){
-            binding.etNome.setText(registro.nome)
-            binding.etTelefone.setText(registro.telefone)
+        val etCodigoPesquisar = EditText(this)
 
-        }else{
-            binding.etNome.setText("")
-            binding.etTelefone.setText("")
-            Toast.makeText(this, "Registro não encontrado", Toast.LENGTH_SHORT).show()
-        }
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Informe o código do registro a ser pesquisado")
+        builder.setView(etCodigoPesquisar)
+        builder.setCancelable(false)
+        builder.setNegativeButton("Fechar", null)
+        builder.setPositiveButton(
+            "Pesquisar",
+            { dialog, which ->
+
+                val registro = banco.pesquisar(etCodigoPesquisar.text.toString().toInt())
+
+                if (registro != null){
+                    binding.etCod.setText(registro._id.toString())
+                    binding.etNome.setText(registro.nome)
+                    binding.etTelefone.setText(registro.telefone)
+
+                }else{
+                    binding.etCod.setText("")
+                    binding.etNome.setText("")
+                    binding.etTelefone.setText("")
+                    Toast.makeText(this, "Registro não encontrado", Toast.LENGTH_SHORT).show()
+                }
+            })
+
+        builder.show()
+
     }
+
     fun btListarOnClick(view: View) {
 
         val intent = Intent(this, ListarActivity::class.java)
